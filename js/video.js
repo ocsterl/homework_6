79abc18f802ed370cb5708ffcffed0329a6520f7
Olivia Sterling <oliviasterling@Olivias-MacBook-Pro-5.local>
2020-11-17 14:02:15 -0500

    updates

--- js/video.js
+++ js/video.js
@@ -6,9 +6,22 @@ window.addEventListener("load", function() {
 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
+	video.play();
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
-	
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate = 0.5;
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate = 2.0;
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	video.onseeking = function();
 });
 
