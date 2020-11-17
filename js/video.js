d4a7db98011e05f42d290f38ff8fb534e1845824
Olivia Sterling <oliviasterling@Olivias-MacBook-Pro-5.local>
2020-11-17 14:43:02 -0500

    changes to all

--- js/video.js
+++ js/video.js
@@ -7,21 +7,54 @@ window.addEventListener("load", function() {
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play();
+	document.querySelector("#volume").innerHTML = "100%";
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
 	video.pause();
 });
 
+var speed = 1.0;
+
 document.querySelector("#slower").addEventListener("click", function() {
-	video.playbackRate = 0.5;
+	speed = speed - speed*(0.1)
+	video.playbackRate = speed;
+	console.log("Updated Speed:" + speed);
 });
 
 document.querySelector("#faster").addEventListener("click", function() {
-	video.playbackRate = 2.0;
+	speed = speed + speed*(0.1)
+	video.playbackRate = speed;
+	console.log("Updated Speed:" + speed);
 });
 
 document.querySelector("#skip").addEventListener("click", function() {
-	video.onseeking = function();
+	if (video.currentTime + 5.0 > video.duration){
+		video.currentTime = 0
+	}
+	else{
+		video.currentTime = video.currentTime + 5.0;
+	}
 });
 
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted == false){
+		video.muted = true;
+		document.querySelector('#mute').innerHTML = 'Unmute';
+	}
+	else{
+		video.muted = false;
+		document.querySelector('#mute').innerHTML = 'Mute';
+	}
+});
+
+document.querySelector("#volumeSlider").addEventListener("change", function() {
+	console.log(this);
+	console.log(this.value);
+	document.querySelector("#volume").innerHTML = this.value + "%";
+});
+
+document.querySelector('#old').addEventListener('click', function(){
+	video.classList.add("oldTime");
+});
